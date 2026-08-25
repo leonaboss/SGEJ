86dfd4ba18368d008ff6936f0550f679e97e7e8a
leonaboss <unefmsiceme@gmail.com>
2026-08-25 12:35:40 -0400

    Fix migration duplicate column error

--- apps/documentos/migrations/0006_add_contenido_ocr_field.py
+++ apps/documentos/migrations/0006_add_contenido_ocr_field.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='documento',
-            name='contenido_ocr',
-            field=models.TextField(blank=True, null=True),
-        ),
     ]
